199795d291fc82cb7cfd1fcdb4b0a506b8e28efa
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-10-05 00:59:33 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create, :show];
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+
+  post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy"
 end
